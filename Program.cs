7769e48fb80c984c53bb80e0bb75edf13ba5c432
alex-shcherbakov <90749411+alex-shcherbakov@users.noreply.github.com>
2022-09-25 19:30:42 +0300

    Add files via upload
    3-rd laboratory

--- Program.cs
+++ Program.cs
@@ -1,59 +1,50 @@
 ﻿using System;
 
-namespace C_sharp2
+namespace c_sharp_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // мой номер в списке - 32, поэтому вариант 2-й
-            //lab#1
-            double r, u;
-            Console.WriteLine("Enter value of tension : ");
-            string str1 = Console.ReadLine();
-            u = double.Parse(str1);
-            Console.WriteLine("Enter value of resistance : ");
-            string str2 = Console.ReadLine();
-            r = double.Parse(str2);
-
-
-            if (r > 0)
+            //лабораторна 3 варіант 11
+            Console.WriteLine("Enter 4-digit number :");
+            string s = Console.ReadLine();
+            char[] str = s.ToCharArray();
+            string a;
+            Console.WriteLine("your reversed number is:");
+            for(int i = str.Length - 1; i > -1; --i)
             {
-                Console.WriteLine("your answer is :");
-                Console.WriteLine("u = " + u);
-                Console.WriteLine("r = " + r);
-                Console.WriteLine("i = " + u / r);
-
-
+                a = Convert.ToString(str[i]);
+                Console.Write(a);
 
             }
-            else if (r == 1)
+            if (s.Length > 3 )
             {
-                Console.WriteLine("your answer is :");
-                Console.WriteLine(" u = " + u);
-                Console.WriteLine(" r =  " + r);
-                Console.WriteLine(" i = " + u);
+                Console.WriteLine();
+                Console.WriteLine("True");
             }
             else
             {
-                Console.WriteLine(" you entered incorrect data");
-            };
-            // lab#2
-            double n, nn, nk;
-               
-                Console.WriteLine("Enter value of n ");
-                string str3 = Console.ReadLine();
-                n = double.Parse(str3);
-                Console.WriteLine("Enter value of k  ");
-                string str4 = Console.ReadLine();
-                nn = double.Parse(str4);
-                Console.WriteLine("n is " + n);
-                Console.WriteLine("k is " + nn);
-                Console.WriteLine("nk is " + (nn + n));
+                Console.WriteLine();
+                Console.WriteLine("not 4-digit number ");
+            }
 
 
-                Console.WriteLine("result is  " + (Math.Pow(nn, 2) + Math.Pow(-1, nn) * (nn - 1)) / (Math.Pow(nn, 2) + 1));
-            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
