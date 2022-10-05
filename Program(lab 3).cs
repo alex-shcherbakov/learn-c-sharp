e0e6bcc4e994235c53850afd21cf84fb05283b8f
alex-shcherbakov <90749411+alex-shcherbakov@users.noreply.github.com>
2022-10-05 18:17:33 +0300

    Add files via upload

--- Program(lab 3).cs
+++ Program(lab 3).cs
@@ -6,27 +6,32 @@ namespace c_sharp_1
     {
         static void Main(string[] args)
         {
-            //лабораторна 3 варіант 11
-            Console.WriteLine("Enter 4-digit number :");
-            string s = Console.ReadLine();
-            char[] str = s.ToCharArray();
-            string a;
-            Console.WriteLine("your reversed number is:");
-            for(int i = str.Length - 1; i > -1; --i)
+            //3-я лабораторная 11-й вариант
+            Console.WriteLine("запишіть 4-х значне число :");
+            string input = Console.ReadLine();
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            string output = new string(charArray);
+            if (input.Length > 3)
             {
-                a = Convert.ToString(str[i]);
-                Console.Write(a);
-
-            }
-            if (s.Length > 3 )
-            {
+                Console.WriteLine("обернене число :");
+                Console.WriteLine(output);
                 Console.WriteLine();
-                Console.WriteLine("True");
             }
             else
             {
+                Console.WriteLine(" Ви ввели не 4-х значне число");
                 Console.WriteLine();
-                Console.WriteLine("not 4-digit number ");
+            }
+            if (input == output)
+            {
+                Console.WriteLine("Вказані числа однакові");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Числа не однакові");
+                Console.WriteLine();
             }
 
 
