87d6f652b3081da8e8291c3ad7fedf4d216a2b94
alex-shcherbakov <90749411+alex-shcherbakov@users.noreply.github.com>
2022-10-05 12:38:19 +0300

    Add files via upload
    друга лабораторна окремо

---
+++ Program(lab 2).cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace C_sharp2
+{
+    //екземпляр класу
+    //get,set
+    //public,private
+    //конструктор 
+    //властивість 
+   
+   class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // лабораторна №2 варіант 2 
+            double n, nn;
+               
+                Console.WriteLine("Enter value of n ");
+                string str3 = Console.ReadLine();
+                n = double.Parse(str3);
+                Console.WriteLine("Enter value of k  ");
+                string str4 = Console.ReadLine();
+                nn = double.Parse(str4);
+                Console.WriteLine("n is " + n);
+                Console.WriteLine("k is " + nn);
+                Console.WriteLine("nk is " + (nn + n));
+
+
+                Console.WriteLine("result is  " + (Math.Pow(nn, 2) + Math.Pow(-1, nn) * (nn - 1)) / (Math.Pow(nn, 2) + 1));
+            
+
+
+
+
+
+        }
+    }
+}
